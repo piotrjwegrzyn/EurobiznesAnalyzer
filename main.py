b0754ce3a0dea0a65e4270d99e599cbcb323f46c
Jan Czech <czechumennakompie@gmail.com>
2021-03-03 22:38:40 +0100

    lecimy
    nie śpimy

--- main.py
+++ main.py
@@ -1,16 +1,65 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class player:
+    playerPos = 1
+    isInPrison = False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class game:
+    list <fieldXD> fields
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class fieldXD:
+    fieldNumber
+
+
+class miasto(fieldXD):
+    value
+    cityName
+
+    def setValues(self, cost, name):
+        self.value = cost
+        self.cityName = name
+
+class blueChance(fieldXD):
+    def randomizeBlue(this):
+        case = random.randint(1, 16)
+
+        if(case > 3):
+            pass
+        elif(case == 1):
+            this.pozycjaGracza = 1
+        elif(case == 2):
+            this.pozycjaGracza = 40
+        elif(case == 3):
+            this.pozycjaGracza = 10
+            this.isInPrison = True
+
+
+class redChance(fieldXD):
+    def randomizeRed(this):
+        case = random.randint(1,16)
+
+        if(case > 7):
+            pass
+        elif(case == 1):
+            this.pozycjaGracza = ten.pozycjaGracza - 3
+        elif(case == 2):
+            this.pozycjaGracza = 1
+        elif(case == 3):
+            this.pozycjaGracza = 10
+        elif(case == 4):
+            this.pozycjaGracza = 36
+        elif(case == 5):
+            this.pozycjaGracza = 15
+        elif(case == 6):
+            this.pozycjaGracza = 24
+        elif(case == 7):
+            this.pozycjaGracza = 7
+
+class cornerFields(fieldXD):
+    def changePos(self, this):
+        if(self.fieldNumber != 31):
+            pass
+        elif(self.fieldNumber == 31):
+            this.playerPos = 10
+            this.isInPrison = True
